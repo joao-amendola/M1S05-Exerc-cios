955c7b55d00aff04a8616fdbe85b55dc39dfbeb3
João Amendola <mx.joaoo@gmail.com>
2022-11-04 20:06:57 -0300

    New way to validate in constructor

--- exercicio-2/Fatura.mjs
+++ exercicio-2/Fatura.mjs
@@ -1,9 +1,11 @@
 export default class Fatura {
     constructor(id, descricao, quantia, preco) {
-        this.id = id;
-        this.descricao = descricao;
-        quantia > 0 ? this.quantia = quantia : quantia = 0;
-        preco > 0 ? this.preco = preco : preco = 0;
+        this.id = id
+        this.descricao = descricao
+        // quantia > 0 ? this.quantia = quantia : quantia = 0;
+        // preco > 0 ? this.preco = preco : preco = 0;
+        this.quantia = quantia > 0 ? quantia : 0
+        this.preco = preco > 0 ? preco : 0
     }
     
     get obterValorTotal () {
